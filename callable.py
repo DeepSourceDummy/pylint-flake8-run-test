555c9c46ad0e423a0f30644172c547abd9174446
Srijan Saurav <sauravsrijan@gmail.com>
2019-05-30 17:21:24 +0530

    Changes

--- callable.py
+++ callable.py
@@ -3,8 +3,10 @@ class Dummy:
         self.vara = None
 
     def get_vara():
-        return self.vara
-
+        return self.vraa
+    
+    def set_vara(self):
+        return "foo"
 
 def my_listener(obj):
     pass
